a5acd2e2b0ab752bc327b0603df36f113a348b4c
唐海 <280503392@qq.com>
2021-07-30 17:39:53 +0800

    build: 打包

--- lib/locale/index.d.ts
+++ lib/locale/index.d.ts
@@ -3,53 +3,7 @@ export interface IGlobalLocal {
     message: any;
 }
 export declare function initI18n(lang: Lang): {
-    confirm: string;
-    cancel: string;
-    loading: string;
-    pageTotal: string;
-    noPermission: string;
-    styleSetting: string;
-    darkSetting: string;
-    showTabSetting: string;
-    fixedTabSetting: string;
-    more: string;
-    logout: string;
-    logoutTitle: string;
-    logoutMessage: string;
-    closeOther: string;
-    refreshPage: string;
-} | {
-    confirm: string;
-    cancel: string;
-    loading: string;
-    pageTotal: string;
-    noPermission: string;
-    styleSetting: string;
-    darkSetting: string;
-    showTabSetting: string;
-    fixedTabSetting: string;
-    more: string;
-    logout: string;
-    logoutTitle: string;
-    logoutMessage: string;
-    closeOther: string;
-    refreshPage: string;
-} | {
-    confirm: string;
-    cancel: string;
-    loading: string;
-    pageTotal: string;
-    noPermission: string;
-    styleSetting: string;
-    darkSetting: string;
-    showTabSetting: string;
-    fixedTabSetting: string;
-    more: string;
-    logout: string;
-    logoutTitle: string;
-    logoutMessage: string;
-    closeOther: string;
-    refreshPage: string;
+    message: any;
 };
 export declare function setLocale(lang: Lang): void;
 export declare function getEASIText(key: string, value?: {
