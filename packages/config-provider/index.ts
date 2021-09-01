312a1e0f0a693c3890a71683820bcbe8946ae9bb
唐海 <280503392@qq.com>
2021-09-01 17:44:11 +0800

    build: 重新构建

--- packages/config-provider/index.ts
+++ packages/config-provider/index.ts
@@ -1,4 +1,4 @@
-import { App } from "vue";
+import {App, Plugin} from "vue";
 import ConfigProvider from "./Index.vue";
 import "tailwindcss/tailwind.css";
 
@@ -6,4 +6,4 @@ ConfigProvider.install = (app: App): void => {
   app.component(ConfigProvider.name, ConfigProvider);
 };
 
-export default ConfigProvider;
+export default ConfigProvider as typeof ConfigProvider & Plugin;
