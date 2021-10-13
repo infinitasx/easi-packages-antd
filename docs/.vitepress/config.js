625b73c5da1d66fdc3a8bc0405cf683da45d1ab2
huyanhai <810839700@qq.com>
2021-10-13 15:18:59 +0800

    chore: 📖 更新website配置文件

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,20 @@
+const mdPlugin = require('./plugins');
+const config = require('../config/node/index');
+
+module.exports = {
+  title: config.title,
+  themeConfig: config.themeConfig,
+  head: config.head,
+  base: config.base || '/',
+  vite: {
+    server: {
+      host: true,
+    },
+  },
+  vue: {},
+  markdown: {
+    config: md => {
+      mdPlugin(md);
+    },
+  },
+};
---
+++ docs/.vitepress/theme/index.js
@@ -0,0 +1,33 @@
+import {
+  ElDrawer,
+  ElButton,
+  ElPopover,
+  ElTooltip,
+  ElCollapseTransition,
+  ElRow,
+} from 'element-plus';
+import initPlugins from '../../config/client';
+
+import 'element-plus/theme-chalk/index.css';
+import 'highlight.js/styles/arduino-light.css';
+import '../scss/index.scss';
+import 'normalize.css';
+import DefaultTheme from 'vitepress/dist/client/theme-default';
+
+import Layout from '../vitepress/Layout.vue';
+import Demo from '../vitepress/Demo.vue';
+
+export default {
+  ...DefaultTheme,
+  Layout: Layout,
+  enhanceApp({ app }) {
+    initPlugins(app);
+    app.use(ElDrawer);
+    app.use(ElButton);
+    app.use(ElPopover);
+    app.use(ElTooltip);
+    app.use(ElCollapseTransition);
+    app.use(ElRow);
+    app.component('Demo', Demo);
+  },
+};
