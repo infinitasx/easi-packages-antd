59ff83f9329846d185ce393da2b57e0981a571a3
huyanhai <810839700@qq.com>
2021-10-26 17:09:44 +0800

    docs: 完善使用文档md示例

--- docs/examples/table/index.vue
+++ docs/examples/table/index.vue
@@ -1,11 +1,13 @@
 <template>
-  <EASITable :dataSource="tableData" rowKey="id" :scroll="{ x: true }">
+  <EASITable :dataSource="tableData" rowKey="id" :scroll="{ x: true }" class="table">
     <template #headerLeft>左侧标题</template>
     <template #headerRight>
       <EASIButton type="primary">操作</EASIButton>
     </template>
-    <a-table-column title="id" key="id" data-index="id" />
-    <a-table-column title="name" key="name" data-index="name" />
+    <a-table-column title="key" key="key" data-index="key" width="20%" />
+    <a-table-column title="name" key="name" data-index="name" width="20%" />
+    <a-table-column title="age" key="age" data-index="age" width="20%" />
+    <a-table-column title="address" key="address" data-index="address" width="20%" />
   </EASITable>
 </template>
 
@@ -13,10 +15,36 @@
 import { defineComponent, ref } from 'vue';
 export default defineComponent({
   setup() {
-    const tableData = ref([{ name: 'EASI', id: 1 }]);
+    const tableData = ref([
+      {
+        key: '1',
+        name: 'John Brown',
+        age: 32,
+        address: 'New York No. 1 Lake Park, New York No. 1 Lake Park',
+        tags: ['nice', 'developer'],
+      },
+      {
+        key: '2',
+        name: 'Jim Green',
+        age: 42,
+        address: 'London No. 2 Lake Park, London No. 2 Lake Park',
+        tags: ['loser'],
+      },
+      {
+        key: '3',
+        name: 'Joe Black',
+        age: 32,
+        address: 'Sidney No. 1 Lake Park, Sidney No. 1 Lake Park',
+        tags: ['cool', 'teacher'],
+      },
+    ]);
     return { tableData };
   },
 });
 </script>
 
-<style></style>
+<style>
+.table {
+  width: 100%;
+}
+</style>
